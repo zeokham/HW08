7076f0ee7fde89aad89494704337dbd7f6c59863
zeokham <100818900+zeokham@users.noreply.github.com>
2022-05-19 12:29:21 +0700

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+	int arr [5];
+	arr [0]=10;
+	arr[1]=20;
+	arr[2]=30;
+	arr[3]=40;
+	arr[4]=50;
+	for (int i=0;i<5;i++)
+	{
+    	printf ("%d\n", arr[i]);
+	}
+	return 0;
+}
